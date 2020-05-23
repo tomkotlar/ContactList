8b2592d4c974e7150be881d4db4a6cd0ae93ecbe
tomkotlar <t.kotlar@yahoo.com>
2020-05-23 19:29:45 +0100

    updating modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 
 const routes: Routes = [
-  {path: '', component: MainNavComponent}
+  {path: '', component: MainNavComponent},
+  {path: 'contacts', component: SideNavComponent},
+  {path: 'contacts/:id', component: SideNavComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,41 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {  HttpClientModule} from "@angular/common/http";
+import { LayoutModule } from '@angular/cdk/layout';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { MaterialModule } from './shared/material.module';
 import { MainComponent } from './main/main.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
+import { UserService } from './services/user.service';
+import { NotesComponent } from './notes/notes.component';
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     MainNavComponent,
-    SideNavComponent
+    SideNavComponent,
+    NewContactComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LayoutModule,
+
   ],
-  providers: [],
+  entryComponents: [NewContactComponent],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
