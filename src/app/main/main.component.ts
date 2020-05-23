4669460d5e9a470e86c01980f584f403a3815ff6
tomkotlar <t.kotlar@yahoo.com>
2020-05-23 18:52:52 +0100

    main adding main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { User } from '../models/user';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"],
 })
 export class MainComponent implements OnInit {
+  user: User;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private service: UserService
+  ) {}
 
-  constructor() { }
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      let id = params[`id`];
+      console.log(id, "hello", params[`id`]);
+      if (!id) id = 1;
+      this.user = null;
 
-  ngOnInit(): void {
+      this.service.users.subscribe((users) => {
+        if (users.length == 0) return;
+        this.user = this.service.userById(id);
+      });
+    });
   }
-
 }
